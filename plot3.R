835ea6a876211cface5db6deb30d722d208c88ba
jeade <j_eade@hotmail.com>
2016-04-23 15:02:16 -0500

    Added files via upload

---
+++ plot3.R
@@ -0,0 +1,40 @@
+# Exploratory Data Analysis,  week1 project
+# Plot 1
+# Author: Jeremy Eade
+# Date: 4/23/2016
+
+# Clear-out objects
+rm(list=ls())
+plot.new()
+
+# Dependent libraries
+library(dplyr)
+library(tidyr)
+library(lubridate)
+
+# Read data and select dates 2007-02-01 and 2007-02-0
+colClasses <- c("character","character","numeric","numeric","numeric","numeric","numeric","numeric","numeric")
+df <- read.table(file="household_power_consumption.txt",header=TRUE,sep=";",dec=".",na.strings="?",colClasses=colClasses)
+df <- tbl_df(df)
+feb2007 <- filter(df,Date=="1/2/2007" | Date=="2/2/2007")
+
+# Create date time from character Date and Time columns
+feb2007 <- mutate(feb2007,dayTime=dmy_hms(paste(Date,Time)))
+
+# Make the plot
+with(feb2007,plot(dayTime,Sub_metering_1,type="n",xlab="",ylab="Energy sub metering"))
+with(feb2007,lines(dayTime,Sub_metering_1))
+with(feb2007,lines(dayTime,Sub_metering_2,col="red"))
+with(feb2007,lines(dayTime,Sub_metering_3,col="blue"))
+legend("topright",lwd=c(1,1,1),col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+
+# Make the PNG file
+png(filename="plot3.png", width=480, height=480)
+with(feb2007,plot(dayTime,Sub_metering_1,type="n",xlab="",ylab="Energy sub metering"))
+with(feb2007,lines(dayTime,Sub_metering_1))
+with(feb2007,lines(dayTime,Sub_metering_2,col="red"))
+with(feb2007,lines(dayTime,Sub_metering_3,col="blue"))
+legend("topright",lwd=c(1,1,1),col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+
+dev.off()
+
